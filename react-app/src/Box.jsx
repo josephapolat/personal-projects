ae08562bf6d0a96bca87bf15afcf510bf54d5829
Joseph Polat <josephaydinpolat@gmail.com>
2026-02-20 23:43:33 -0500

    Added basic button component

---
+++ react-app/src/Box.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css'; // Import the CSS file
+
+const Box = () => {
+  return (
+    <div className="simple-box">
+      HOME
+    </div>
+  );
+};
+
+export default Box;
